810ca3836e3007a54514f09968f5c161faf2a7d5
MichaelBurns@mac.com <MichaelBurns@mac.com>
2024-05-07 21:35:11 +0000

    New celebration post and how to do time estimates

--- .data/wp_commentmeta.sql
+++ .data/wp_commentmeta.sql
@@ -30,7 +30,7 @@ CREATE TABLE `wp_commentmeta` (
   PRIMARY KEY (`meta_id`),
   KEY `comment_id` (`comment_id`),
   KEY `meta_key` (`meta_key`(191))
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `wp_commentmeta` (
 
 LOCK TABLES `wp_commentmeta` WRITE;
 /*!40000 ALTER TABLE `wp_commentmeta` DISABLE KEYS */;
-INSERT INTO `wp_commentmeta` VALUES (3,10,'_wp_trash_meta_status','0'),(4,10,'_wp_trash_meta_time','1712766032'),(5,9,'_wp_trash_meta_status','0'),(6,9,'_wp_trash_meta_time','1712766034'),(7,11,'_wp_trash_meta_status','0'),(8,11,'_wp_trash_meta_time','1713454642'),(9,12,'_wp_trash_meta_status','0'),(10,12,'_wp_trash_meta_time','1713454644'),(11,14,'_wp_trash_meta_status','0'),(12,14,'_wp_trash_meta_time','1713968409'),(13,13,'_wp_trash_meta_status','0'),(14,13,'_wp_trash_meta_time','1713968411');
+INSERT INTO `wp_commentmeta` VALUES (3,10,'_wp_trash_meta_status','0'),(4,10,'_wp_trash_meta_time','1712766032'),(5,9,'_wp_trash_meta_status','0'),(6,9,'_wp_trash_meta_time','1712766034'),(7,11,'_wp_trash_meta_status','0'),(8,11,'_wp_trash_meta_time','1713454642'),(9,12,'_wp_trash_meta_status','0'),(10,12,'_wp_trash_meta_time','1713454644'),(11,14,'_wp_trash_meta_status','0'),(12,14,'_wp_trash_meta_time','1713968409'),(13,13,'_wp_trash_meta_status','0'),(14,13,'_wp_trash_meta_time','1713968411'),(15,17,'_wp_trash_meta_status','0'),(16,17,'_wp_trash_meta_time','1714822838'),(17,16,'_wp_trash_meta_status','0'),(18,16,'_wp_trash_meta_time','1714822839'),(19,15,'_wp_trash_meta_status','0'),(20,15,'_wp_trash_meta_time','1714822841'),(21,18,'_wp_trash_meta_status','0'),(22,18,'_wp_trash_meta_time','1715079274'),(23,19,'_wp_trash_meta_status','0'),(24,19,'_wp_trash_meta_time','1715079330');
 /*!40000 ALTER TABLE `wp_commentmeta` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-25 22:08:52
+-- Dump completed on 2024-05-07 21:31:22
