f36c6e240e941e551762e453fdd168da89bd33fd
MichaelBurns@mac.com <MichaelBurns@mac.com>
2024-08-31 19:20:12 +0000

    update jetpack

---
+++ wp-content/jetpack-waf/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+define( 'DISABLE_JETPACK_WAF', false );
+if ( defined( 'DISABLE_JETPACK_WAF' ) && DISABLE_JETPACK_WAF ) return;
+define( 'JETPACK_WAF_MODE', 'silent' );
+define( 'JETPACK_WAF_SHARE_DATA', false );
+define( 'JETPACK_WAF_SHARE_DEBUG_DATA', false );
+define( 'JETPACK_WAF_DIR', '/home/971999.cloudwaysapps.com/hynxgrbyjv/public_html/wp-content/jetpack-waf' );
+define( 'JETPACK_WAF_WPCONFIG', '/home/971999.cloudwaysapps.com/hynxgrbyjv/public_html/wp-content/../wp-config.php' );
+require_once '/home/971999.cloudwaysapps.com/hynxgrbyjv/public_html/wp-content/plugins/jetpack/vendor/autoload.php';
+Automattic\Jetpack\Waf\Waf_Runner::initialize();
