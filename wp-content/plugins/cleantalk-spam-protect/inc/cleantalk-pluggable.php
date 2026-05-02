4780e84668b5a63be5281746c541dcc0844e5891
MichaelBurns@mac.com <MichaelBurns@mac.com>
2026-05-02 18:19:33 +0000

    plugins and themes updates

--- wp-content/plugins/cleantalk-spam-protect/inc/cleantalk-pluggable.php
+++ wp-content/plugins/cleantalk-spam-protect/inc/cleantalk-pluggable.php
@@ -723,6 +723,7 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
             'fl_builder_subscribe_form_submit', // FLBuilderForms has direct integration
             'tutor_pro_social_authentication', // Tutor Pro social authentication, we trust a third-party service
             'drplus_login', // Doctor Plus theme login
+            'fluent_cal_schedule_meeting', // Fluent Booking has direct integration
         );
 
         // Skip test if
@@ -1406,6 +1407,13 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
         }
 
         if (
+            apbct_is_plugin_active('booking/wpdev-booking.php') &&
+            (Post::getString('action') === 'WPBC_AJX_BOOKING__CREATE')
+        ) {
+            return 'WP BookingCalendar service action';
+        }
+
+        if (
             (
                 apbct_is_plugin_active('pixelyoursite/pixelyoursite.php') ||
                 apbct_is_plugin_active('pixelyoursite-pro/pixelyoursite-pro.php')
@@ -1429,7 +1437,11 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
                 apbct_is_plugin_active('piotnet-addons-for-elementor-pro/piotnet-addons-for-elementor-pro.php') ||
                 apbct_is_plugin_active('piotnet-addons-for-elementor/piotnet-addons-for-elementor.php')
             ) &&
-            Post::get('action') === 'pafe_ajax_form_builder_preview_submission' ) {
+            (
+                Post::get('action') === 'pafe_ajax_form_builder_preview_submission' ||
+                Post::get('action') === 'pafe_ajax_form_builder'
+            )
+        ) {
             return 'PAFE';
         }
 
@@ -1438,7 +1450,7 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
             apbct_is_plugin_active('bloom/bloom.php') &&
             Post::get('action') === 'bloom_subscribe'
         ) {
-            return 'Bloom';
+            return 'Bloom skip - has the direct integration';
         }
 
         // Ajax Search Lite - these requests will be caught by search form protection
@@ -1806,6 +1818,11 @@ function apbct_is_skip_request($ajax = false, $ajax_message_obj = array())
         if ( apbct_is_plugin_active('woocommerce/woocommerce.php') &&
              apbct_is_in_uri('wc-ajax=iwd_opc_update_order_review') ) {
             return 'cartflows_save_cart';
+        }
+        // WC addon - Metorik Helper plugin service requests
+        if ( apbct_is_plugin_active('metorik-helper/metorik-helper.php') &&
+             apbct_is_in_uri('wc-ajax=metorik_capture_customer_data') ) {
+            return 'metorik-helper skip';
         }
         // Vault Press (JetPack) plugin service requests
         if (
